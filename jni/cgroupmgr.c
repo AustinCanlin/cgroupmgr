535e4d4f23c2aba227fd622745f7f237fb5749fc
CodeMunki <CodeMunki@1ab5e088-6fe0-0e68-615c-5833e97db6f4>
2010-07-16 20:41:21 +0000

    Added process info.

--- jni/cgroupmgr.c
+++ jni/cgroupmgr.c
@@ -4,19 +4,21 @@
 #include <jni.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#define MAX_PATH 1024
 
 // Puts list of processes in linked list.  Returns number of entries
 int getDirList(char *szDir, list_node **pList);
 
+void getProcessName(char *szPID, char *szProcInfo);
+
+int getFileSize(char *szFile);
+
 int isNumber(char *szStr);
 
-jstring
-Java_steve_cgroups_CGroupInfo_stringFromJNI( JNIEnv* env,
-                                                  jobject thiz )
-{
-    return (*env)->NewStringUTF(env, "This might just work.");
-}
-
+// Returns a process list to the Java application
 jobjectArray Java_steve_cgroups_CGroupInfo_getProcessList( JNIEnv* env,
                                                   jobject thiz)
 {	
@@ -37,9 +39,11 @@ jobjectArray Java_steve_cgroups_CGroupInfo_getProcessList( JNIEnv* env,
  	
    	for(pCur = pList; pCur != NULL; pCur = pCur->pNext)
    	{
-   		char szStr[20];
-   		sprintf(szStr, "Hello %d", i);
-		jstring str = (*env)->NewStringUTF(env, (char*)(pCur->pPayload));		
+   		char procInfo[MAX_PATH + 1];
+   		
+   		getProcessName((char*)(pCur->pPayload), procInfo);
+   		
+		jstring str = (*env)->NewStringUTF(env, procInfo);		
 		(*env)->SetObjectArrayElement(env, processList, i, str);
 
 		i++;
@@ -51,6 +55,32 @@ jobjectArray Java_steve_cgroups_CGroupInfo_getProcessList( JNIEnv* env,
   	return processList;
 }
 
+// Returns the statistical information for a running process
+jstring Java_steve_cgroups_CGroupInfo_getProcessInfo( JNIEnv* env,
+                                                  jobject thiz, jstring pid)
+{
+	char szProcInfoPath[MAX_PATH+1];
+	jstring strRet = NULL;
+	
+	sprintf(szProcInfoPath, "/proc/%s/status", (*env)->GetStringChars(env, pid, 0));
+	
+	char szInfo[(MAX_PATH*2) + 1];
+	
+	FILE *fileStat = fopen(szProcInfoPath, "r");
+	
+	if(NULL != fileStat)
+	{
+		unsigned long ulSize = fread(szInfo, 1, MAX_PATH*2, fileStat);
+		
+		strRet = (*env)->NewStringUTF(env, szInfo);
+		
+		fclose(fileStat);
+	}
+	
+	return strRet;
+}
+
+// Loads the list of files/directories in a directory into a linked list
 int getDirList(char *szDir, list_node **pList)
 {
 	DIR *dp;
@@ -79,6 +109,67 @@ int getDirList(char *szDir, list_node **pList)
     return i;
 }
 
+// Returns a process name to be shown in the process list
+void getProcessName(char *szPID, char *szProcInfo)
+{
+	char szProcInfoPath[MAX_PATH + 1];
+	int nGotInfo = 0;
+	
+	sprintf(szProcInfo, "%s ", szPID);
+	
+	// Set up base path for proc info
+	sprintf(szProcInfoPath, "/proc/%s", szPID);
+	
+	char szCmdLineFile[MAX_PATH + 1];
+	
+	// Create full path to the /proc/PID/cmdline file
+	sprintf(szCmdLineFile, "%s/cmdline", szProcInfoPath);
+	
+	char *szInfo = (char*)malloc(MAX_PATH + 1);
+	
+	FILE *cmdFile = fopen(szCmdLineFile, "r");
+	
+	if(NULL != cmdFile)
+	{
+		if(0 != fread(szInfo, 1, MAX_PATH, cmdFile))
+		{	
+			strncat(szProcInfo, szInfo, MAX_PATH);
+			nGotInfo = 1;
+		}
+		
+		fclose(cmdFile);
+	}
+	
+	// No command line available, get name from /proc/PID/stat
+	if(0 == nGotInfo)
+	{
+		char szStatFile[MAX_PATH + 1];
+		
+		sprintf(szStatFile, "%s/stat", szProcInfoPath);
+		
+		FILE *statFile = fopen(szStatFile, "r");
+	
+		if(NULL != statFile)
+		{
+			char szStats[MAX_PATH+1];
+			
+			if(0 != fread(szStats, 1, MAX_PATH, cmdFile))
+			{
+				char *szTok = strtok(szStats, " ");
+				
+				szTok = strtok(NULL, " ");
+				
+				strncat(szProcInfo, szTok, MAX_PATH);
+				
+				nGotInfo = 1;
+			}
+			
+			fclose(statFile);
+		}
+	}
+}
+
+// Returns 1 if the string is a number, 0 otherwise
 int isNumber(char *szStr)
 {
 	int nRet = 1;
@@ -94,4 +185,13 @@ int isNumber(char *szStr)
 	}
 	
 	return nRet;
+}
+
+// Returns the size, in bytes, of a file
+int getFileSize(char *szFile)
+{
+	struct stat file_status;
+	
+	stat(szFile, &file_status);
+	return file_status.st_size;
 }
--- src/steve/cgroups/ProcessListView.java
+++ src/steve/cgroups/ProcessListView.java
@@ -4,8 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.content.Intent;
+import java.util.StringTokenizer;
 
-public class ProcessListView extends Activity 
+public final class ProcessListView extends Activity 
 {
 	@Override
     public void onCreate(Bundle savedInstanceState) 
@@ -14,6 +20,16 @@ public class ProcessListView extends Activity
         //setContentView(R.layout.main);
         
         ListView  lv = new ListView(this);
+        
+        lv.setOnItemClickListener(new OnItemClickListener() 
+        {
+            public void onItemClick(AdapterView<?> parent, View view,
+                int position, long id) 
+            {
+            	String text = ((TextView) view).getText().toString();
+            	onListItemSelected(view, text);              
+            }
+        });
         
         CGroupInfo cgInfo = new CGroupInfo();
         
@@ -27,4 +43,24 @@ public class ProcessListView extends Activity
         lv.setAdapter(procAdapter);
         setContentView(lv);
     }  
+	
+	void onListItemSelected(View view, String itemText)
+	{
+		StringTokenizer strTok = new StringTokenizer(itemText, " ");
+		
+		if(strTok.hasMoreTokens())
+		{
+			String strPID = strTok.nextToken();
+			
+			Intent i = new Intent(this, ProcessInfoView.class);
+			
+			Bundle b = new Bundle();
+
+			b.putString("PID", strPID);
+
+			i.putExtras(b);
+			
+			startActivityForResult(i, 0);
+		}
+	}
 }

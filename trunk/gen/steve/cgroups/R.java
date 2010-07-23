e052c34a684dce593ffcd1c08869f9b8d9258fb0
CodeMunki <CodeMunki@1ab5e088-6fe0-0e68-615c-5833e97db6f4>
2010-07-23 03:52:23 +0000

    Added the ability to browse the cgroup hierarchy and to add children to a cgroup.

--- trunk/gen/steve/cgroups/R.java
+++ trunk/gen/steve/cgroups/R.java
@@ -14,16 +14,18 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int TextView01=0x7f050000;
-        public static final int TextView02=0x7f050002;
-        public static final int TextView03=0x7f050004;
-        public static final int btnOpenProcList=0x7f050001;
-        public static final int cgroupChildSpinner=0x7f050005;
-        public static final int txtCGroupInfo=0x7f050006;
+        public static final int TextView02=0x7f050000;
+        public static final int btnAdd=0x7f050002;
+        public static final int btnAddChild=0x7f050006;
+        public static final int btnOpenChildList=0x7f050004;
+        public static final int btnOpenProcList=0x7f050005;
+        public static final int txtCGroupInfo=0x7f050007;
         public static final int txtCGroupPath=0x7f050003;
+        public static final int txtChildName=0x7f050001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int add_child=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- trunk/src/steve/cgroups/ProcessListView.java
+++ trunk/src/steve/cgroups/ProcessListView.java
@@ -13,11 +13,28 @@ import java.util.StringTokenizer;
 
 public final class ProcessListView extends Activity 
 {
+	private Bundle bundle;
+	private String action = "info";
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
+        
+        Intent i = getIntent();
+        
+        if(null != i)
+        {
+	        bundle = i.getExtras();
+	     
+	        if(null != bundle)
+	        {
+		        if(null != bundle.getString("action"))
+		        {
+		        	action = bundle.getString("action");        	
+		        }
+	        }
+        }
         
         ListView  lv = new ListView(this);
         
@@ -48,19 +65,35 @@ public final class ProcessListView extends Activity
 	{
 		StringTokenizer strTok = new StringTokenizer(itemText, " ");
 		
+		String strPID = strTok.nextToken();
 		if(strTok.hasMoreTokens())
 		{
-			String strPID = strTok.nextToken();
+			if(0 == action.compareToIgnoreCase("info"))
+			{
+				Intent i = new Intent(this, ProcessInfoView.class);
+				
+				if(null == bundle)
+					bundle = new Bundle();
+	
+				bundle.putString("myPID", strPID);
+	
+				i.putExtras(bundle);
 			
-			Intent i = new Intent(this, ProcessInfoView.class);
-			
-			Bundle b = new Bundle();
-
-			b.putString("myPID", strPID);
-
-			i.putExtras(b);
-			
-			startActivityForResult(i, 0);
+				startActivityForResult(i, 0);
+			}
+			else if(0 == action.compareToIgnoreCase("select"))
+			{
+				Intent intent = new Intent();
+				
+				Bundle b = new Bundle();
+				
+				b.putString("myPID", strPID);
+				
+				intent.putExtras(b);
+				
+				setResult(CGroupMgr.SELECT_PROCESS_REQUEST, intent);
+				finish();
+			}
 		}
 	}
 }
